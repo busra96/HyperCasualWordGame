dba792192b41af6c5c87095d6a90697755857d6e
busrasimsek <busrasimsek929@gmail.com>
2023-12-11 15:31:47 +0300

    improving the letter hint

--- WordGame/Assets/Word Finder Main/Scripts/HintManager.cs
+++ WordGame/Assets/Word Finder Main/Scripts/HintManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -62,13 +63,30 @@ public class HintManager : MonoBehaviour
 
     }
 
+    private List<int> letterHintGivenIndices = new List<int>();
     public void LetterHint()
     {
+        if (letterHintGivenIndices.Count >= 5)
+        {
+            Debug.Log(" All hints have been given ");
+            return;
+        }
+            
+
+        List<int> letterHintNotGivenIndices = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            if(!letterHintGivenIndices.Contains(i))
+                letterHintNotGivenIndices.Add(i);
+        }
+
         WordContainer currentWordContainer = InputManager.instance.GetCurrentWordContainer();
 
         string secretWord = WordManager.instance.GetSecretWord();
 
-        int randomIndex = Random.Range(0, secretWord.Length);
+        int randomIndex = letterHintNotGivenIndices[Random.Range(0, letterHintNotGivenIndices.Count)];
+        letterHintGivenIndices.Add(randomIndex);
 
         currentWordContainer.AddAsHint(randomIndex, secretWord[randomIndex]);
 
