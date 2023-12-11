424c637bc7385f1c31ae45148e564d88c9bbc8dd
busrasimsek <busrasimsek929@gmail.com>
2023-12-11 17:10:12 +0300

    getting the secret word from a dictionary

--- WordGame/Assets/Word Finder Main/Scripts/WordManager.cs
+++ WordGame/Assets/Word Finder Main/Scripts/WordManager.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WordManager : MonoBehaviour
@@ -8,10 +5,10 @@ public class WordManager : MonoBehaviour
 
     public static WordManager instance;
 
-
     [Header(" Elements ")]
     [SerializeField] private string secretWord;
-    
+    [SerializeField] private TextAsset wordsText;
+    private string words;
     
     private void Awake()
     {
@@ -19,12 +16,14 @@ public class WordManager : MonoBehaviour
             instance = this;
         else
             Destroy(gameObject);
+
+        words = wordsText.text;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetNewSecretWord();
     }
 
     // Update is called once per frame
@@ -36,6 +35,18 @@ public class WordManager : MonoBehaviour
     public string GetSecretWord()
     {
         return secretWord.ToUpper();
+    }
+
+    private void SetNewSecretWord()
+    {
+        Debug.Log("string lenght : " + words.Length);
+        int wordCount =   (words.Length + 2 ) / 7;
+
+        int wordIndex = Random.Range(0, wordCount);
+
+        int wordStartIndex = wordIndex * 7;
+
+        secretWord = words.Substring(wordStartIndex, 5).ToUpper();
     }
     
 }
