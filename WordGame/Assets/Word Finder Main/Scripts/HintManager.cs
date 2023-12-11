e8661192d90b31365e2a8fa944b0b29a0cac7203
busrasimsek <busrasimsek929@gmail.com>
2023-12-11 11:54:13 +0300

    adding the keyboard hint

--- WordGame/Assets/Word Finder Main/Scripts/HintManager.cs
+++ WordGame/Assets/Word Finder Main/Scripts/HintManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class HintManager : MonoBehaviour
 {
@@ -28,6 +29,37 @@ public class HintManager : MonoBehaviour
 
     public void KeyboardHint()
     {
+        string secretWord = WordManager.instance.GetSecretWord();
+
+        List<KeyboardKey> untouchedKeys = new List<KeyboardKey>();
+
+        for (int i = 0; i < keys.Length; i++)
+        {
+            if (keys[i].IsUntouched())
+            {
+                untouchedKeys.Add(keys[i]);
+            }
+        }
+        
+        //at this point, we have a list of all the untouched keys
+        //Let's remove the ones that are in the secret word to avoid graying them out
+
+        List<KeyboardKey> t_untouchedKeys = new List<KeyboardKey>(untouchedKeys);
+
+        for (int i = 0; i < untouchedKeys.Count; i++)
+        {
+            if (secretWord.Contains(untouchedKeys[i].GetLetter()))
+                t_untouchedKeys.Remove(untouchedKeys[i]);
+        }
+
+        //At this point, we have a list of all the untouched keys, not contained into the secret word
+        
+        if(t_untouchedKeys.Count <= 0)
+            return;
+
+        int randomKeyIndex = Random.Range(0, t_untouchedKeys.Count);
+        t_untouchedKeys[randomKeyIndex].SetInvalid();
+
     }
 
     public void LetterHint()
